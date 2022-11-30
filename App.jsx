24cd214c84fccca6048878f3d563901608b1017b
Rabii Luena <itisrabii@gmail.com>
2022-11-30 22:41:20 +0300

    chore: adding react-native navigation dependencies

--- App.jsx
+++ App.jsx
@@ -1,22 +1,13 @@
 /* eslint-disable react/style-prop-object */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import AppLoader from '~/app/components/AppLoader';
 import Landing from '~/app/screens/Landing';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <AppLoader>
       <Landing />
       <StatusBar style="auto" />
-    </View>
+    </AppLoader>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
