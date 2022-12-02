e181e62360cc135ce749c953ea2bf462488e1986
Rabii Luena <itisrabii@gmail.com>
2022-12-02 17:17:37 +0300

    changing locations for app startup images

---
+++ app.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  name: 'Vidcall',
+  slug: 'vidcall',
+  description: 'Phone call application for everyone.',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/images/icon.png',
+  userInterfaceStyle: 'light',
+  splash: {
+    image: './assets/images/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/images/adaptive-icon.png',
+      backgroundColor: '#FFFFFF',
+    },
+    package: 'com.rbluena.vidcall',
+    permissions: ['CAMERA', 'READ_CONTACTS', 'STORAGE', 'READ_PHONE_STATE'],
+  },
+  plugins: [
+    '@react-native-firebase/app',
+    '@react-native-firebase/auth',
+    [
+      'expo-image-picker',
+      {
+        photosPermission: 'Vidcall would like to access your photos.',
+      },
+    ],
+  ],
+  web: {
+    favicon: './assets/images/favicon.png',
+  },
+};
