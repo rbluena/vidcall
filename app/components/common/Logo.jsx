850eb48dc2dc63ebad9fa72471affc3edb605365
Rabii Luena <itisrabii@gmail.com>
2022-12-03 16:53:28 +0300

    fix: fixing logo image failing to load

--- app/components/common/Logo.jsx
+++ app/components/common/Logo.jsx
@@ -1,12 +1,25 @@
 import { StyleSheet, Image } from 'react-native';
-import imageURI from '~/assets/images/icon_small.png';
+import PropTypes from 'prop-types';
 
-const Logo = () => <Image style={styles.image} source={{ uri: imageURI }} />;
+const logoImage = require('../../../assets/images/icon_510.png');
+
+const Logo = ({ style: externalStyle }) => (
+  <Image style={[styles.image, externalStyle]} source={logoImage} />
+);
+
+Logo.defaultProps = {
+  style: {},
+};
+
+Logo.propTypes = {
+  style: PropTypes.objectOf(PropTypes.any),
+};
 
 export default Logo;
 
 const styles = StyleSheet.create({
-  container: {},
-  image: {},
-  text: {},
+  image: {
+    width: 72,
+    height: 72,
+  },
 });
