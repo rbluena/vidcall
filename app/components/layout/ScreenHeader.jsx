34044bb2f9aaafdbd2947198efb5a0a3392a65d6
Rabii Luena <itisrabii@gmail.com>
2022-12-03 20:20:34 +0300

    feat: adding reusable ScreenHeader component

---
+++ app/components/layout/ScreenHeader.jsx
@@ -0,0 +1,41 @@
+import { StyleSheet, View } from 'react-native';
+import PropTypes from 'prop-types';
+import { Text } from '~/app/components/common';
+
+const ScreenHeader = ({ heading, subheading }) => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text fontSize="xl" style={styles.heading}>
+          {heading}
+        </Text>
+        {subheading?.length > 0 ? (
+          <Text variant="muted">{subheading}</Text>
+        ) : null}
+      </View>
+    </View>
+  );
+};
+
+ScreenHeader.defaultProps = {
+  subheading: '',
+};
+
+ScreenHeader.propTypes = {
+  heading: PropTypes.string.isRequired,
+  subheading: PropTypes.string,
+};
+
+export default ScreenHeader;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16,
+    paddingVertical: 54,
+  },
+  heading: {
+    fontWeight: 'bold',
+  },
+});
